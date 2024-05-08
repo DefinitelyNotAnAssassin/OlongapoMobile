15ef66439fb9b3374257cc18bac4dbf41b4ebc23
DefinitelyNotAnAssassin <dharklike@gmail.com>
2024-05-09 00:50:40 +0800

    Initial Backend Setup

--- app/src/main/java/com/olongapogo/MainActivity.java
+++ app/src/main/java/com/olongapogo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.olongapogo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button signUpBtn;
+    TextView signInText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_form);
+        setContentView(R.layout.activity_main_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        signUpBtn = (Button) findViewById(R.id.btnSignUpMain);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Registration.class);
+                startActivity(intent);
+            }
+        });
+        signInText = (TextView) findViewById(R.id.linkSignin);
+        signInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
         });
     }
 }
